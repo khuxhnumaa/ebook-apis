cf8815007c82ff7c0b035adfff6b5a9335c2b478
Khushnuma Parveen <gp4440@myamu.ac.in>
2026-07-11 01:07:21 +0530

    Add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,8 +2,6 @@ import type { NextFunction, Request, Response } from "express";
 import bcrypt from 'bcrypt'
 import createHttpError from "http-errors";
 import userModel from "./userModel.js";
-// import { sign } from "jsonwebtoken";
-import { User } from "./userTypes.js";
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 const createUser = async (req:Request,res:Response, next:NextFunction)=>{
@@ -34,18 +32,16 @@ const createUser = async (req:Request,res:Response, next:NextFunction)=>{
     }
     const hashPassword = await bcrypt.hash(password,10);
 
-    let newUser = User;
+    // let newUser: User | null = null;
     //now store password -> hash(async)
-    try{
-        newUser = await userModel.create({
-            name,
-            email,
-            password:hashPassword,
-        });
-    }
-    catch(err){
-        return next(createHttpError(500,"Error while hashing password"));
-    }
+    
+    const newUser = await userModel.create({
+        name,
+        email,
+        password:hashPassword,
+    });
+    
+    
     
     //token Generation-JWT system
     // const token = sign({sub:newUser._id},config.jwtSecret as string,{ expiresIn: 60 * 60 });
@@ -57,7 +53,35 @@ const createUser = async (req:Request,res:Response, next:NextFunction)=>{
 
 
     //response
-    res.json({accessToken: token});
+    res.status(201).json({accessToken: token});
 };
 
+const loginUser = async (req:Request,res:Response,next:NextFunction)=>{
+
+    const {email, password}= req.body;
+    if(email || !password){
+        return next(createHttpError(400,"all feilds are required"));
+    }
+
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        return next(createHttpError(401,"User not found"));
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return next(createHttpError(400,"Username or password is incorrect"));
+    }
+    const token = jwt.sign(
+        { sub: user._id },
+        config.jwtSecret as string,
+        {expiresIn: "7d"}
+      );
+    res.json({accessToken: token});
+
+
+    res.json({message:"Login success"});
+}
 export {createUser};
+export {loginUser};
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,9 +1,12 @@
 import express from 'express'
-import { createUser } from './userController.js'
+import { createUser, loginUser } from './userController.js'
 const userRoute = express.Router()
 
 // userRoute.post('/register',(req,res)=>{
 //     res.json({message: "User registered successfully!"})
 // })
-userRoute.post('/register', createUser)
+userRoute.post('/register', createUser);
+
+userRoute.post("/login",loginUser);
+
 export default userRoute;
