a71e55995f05af98fcd97765a638441d7f7693ea
Khushnuma Parveen <gp4440@myamu.ac.in>
2026-07-14 00:11:06 +0530

    setup multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
-
+//for files - form data is send ~ multi part form data
 const createBook = async(
-    req: Request,
-    res: Response, 
-    next: NextFunction)=>{
+    req: Request,res: Response, next: NextFunction)=>{
+
+
         res.json({})
     };
 
